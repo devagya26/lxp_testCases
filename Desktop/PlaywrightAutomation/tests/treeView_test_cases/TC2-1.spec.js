3e91291f424f41ca1e60fffe7f8c19bbcc03998e
Devagya Jangid <devagya.jangid26@gmail.com>
2024-10-16 03:26:11 +0530

    deleted

--- Desktop/PlaywrightAutomation/tests/treeView_test_cases/TC2-1.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require("@playwright/test");
-const { loginCred } = require('./loginCred');
-
-test("LXP treeView_testCase", async ({ page }) => {
-  await loginCred(page);
-
-  await page.locator("//span[normalize-space()='Links Explorer']").click();
-  await page.waitForTimeout(2000);
-});
